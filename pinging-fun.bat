de466d3585bace503d13e7e343787427db07adc5
Liu-Wen-Yuan <15816141883@163.com>
2020-06-13 08:14:56 +0800

    Update pinging-fun.bat

--- pinging-fun.bat
+++ pinging-fun.bat
@@ -1,14 +1,14 @@
 @echo off
 
 rem title
-title 1DDOSER 3RUNTIME by Dobby233Liu (Program for laughs/有趣程序)
+title DDOSer by Dobby233Liu (Program for laughs/有趣程序)
 
-set /p langggddos= 语言/Lang (zh, en) (Really needed/真的需要): 
+set /p langggddos=语言/Language (zh, en) (Really needed/真的需要): 
 
 rem lets go
 
-if "%langggddos%" == "zh" goto main1
-if "%langggddos%" == "en" goto mainen
+if "%langggddos%"=="zh" goto main1
+if "%langggddos%"=="en" goto mainen
 goto invaildlang
 exit
 
@@ -16,16 +16,16 @@ exit
 rem u idk
 
 :main1
-set /p DDOSADDRESS= 地址（URL/IP） :
-if not "%DDOSADDRESS%" == "" goto bddos
+set /p DDOSADDRESS=地址（URL/IP）:
+if not "%DDOSADDRESS%"=="" goto bddos
 echo 请填写地址！
 pause
 cls
 goto main1
 
 :mainen
-set /p DDOSADDRESS= Address (URL/IP): 
-if not "%DDOSADDRESS%" == "" goto bddos
+set /p DDOSADDRESS=Address (URL/IP): 
+if not "%DDOSADDRESS%"=="" goto bddos
 echo Please provide an address!
 pause
 cls
